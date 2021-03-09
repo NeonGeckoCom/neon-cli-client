a39bec253fc2c5fdd06920539af78e32308806fa
Daniel McKnight <34697904+NeonDaniel@users.noreply.github.com>
2021-03-09 12:26:38 -0800

    Bugfixes and Neon Compat. (#1)

--- neon_cli/__main__.py
+++ neon_cli/__main__.py
@@ -18,6 +18,7 @@ import io
 import os.path
 import curses
 import tempfile
+from neon_utils.configuration_utils import NGIConfig
 from neon_cli.text_client import start_log_monitor, start_mic_monitor, \
     connect_to_mycroft, simple_cli, load_settings, ctrl_c_handler, gui_main, \
     save_settings
@@ -38,16 +39,18 @@ sys.excepthook = custom_except_hook  # noqa
 
 def main():
     import argparse
+
+    logs_dir = NGIConfig("ngi_local_conf").content.get("dirVars", {}).get("logsDir", "/var/log/mycroft")
     parser = argparse.ArgumentParser()
-    parser.add_argument("ipc_dir", type=str, help="the base",
+    parser.add_argument("--ipc_dir", dest="ipc_dir", type=str, help="the base",
                         default=os.path.join(tempfile.gettempdir(), "mycroft", "ipc"))
-    parser.add_argument("logs_dir", type=str,
+    parser.add_argument("--logs_dir", dest="logs_dir", type=str,
                         help="directory where mycroft logs are stored",
-                        default="/var/log/mycroft/")
-    parser.add_argument("lang", type=str,
+                        default=logs_dir)
+    parser.add_argument("--lang", dest="lang", type=str,
                         help="lang code",
                         default="en-us")
-    parser.add_argument("simple", type=bool,
+    parser.add_argument("--simple", dest="simple", type=bool,
                         help="use simple cli without logs",
                         default=False)
     args = parser.parse_args()
@@ -55,7 +58,8 @@ def main():
     # Monitor system logs
     start_log_monitor(os.path.join(args.logs_dir, 'skills.log'))
     start_log_monitor(os.path.join(args.logs_dir, 'voice.log'))
-
+    start_log_monitor(os.path.join(args.logs_dir, 'audio.log'))
+    start_log_monitor(os.path.join(args.logs_dir, 'extras.log'))
     # Monitor IPC file containing microphone level info
     start_mic_monitor(os.path.join(args.ipc_dir, "mic_level"))
 
