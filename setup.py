240d0a022b6a20e4cabf7e01960e33e6707f0537
Daniel McKnight <34697904+NeonDaniel@users.noreply.github.com>
2021-05-28 13:02:06 -0700

    Update requirements and parsing in setup.py (#6)
    * Update requirements and parsing in setup.py
    
    * Fix malformed requirements spec for neon_utils
    
    * Update requirements to exclude LGPL licensed package
    Fix requirements parsing to handle github deps with no auth

--- setup.py
+++ setup.py
@@ -14,17 +14,37 @@
 # Authors: Guy Daniels, Daniel McKnight, Regina Bloomstine, Elon Gasper, Richard Leeds
 
 from setuptools import setup
+from os import path, getenv
+
+
+def get_requirements(requirements_filename: str):
+    requirements_file = path.join(path.abspath(path.dirname(__file__)), "requirements", requirements_filename)
+    with open(requirements_file, 'r', encoding='utf-8') as r:
+        requirements = r.readlines()
+    requirements = [r.strip() for r in requirements if r.strip() and not r.strip().startswith("#")]
+
+    for i in range(0, len(requirements)):
+        r = requirements[i]
+        if "@" in r:
+            parts = [p.lower() if p.strip().startswith("git+http") else p for p in r.split('@')]
+            r = "@".join(parts)
+            if getenv("GITHUB_TOKEN"):
+                if "github.com" in r:
+                    r = r.replace("github.com", f"{getenv('GITHUB_TOKEN')}@github.com")
+            requirements[i] = r
+    return requirements
+
 
 setup(
     name='neon_cli_client',
-    version='0.0.5',
+    version='0.0.6',
     packages=['neon_cli'],
     url='https://github.com/NeonGeckoCom/neon_cli',
     license='apache-2.0',
     author='jarbasAi',
     author_email='',
     description='cli client extracted from mycroft-core',
-    install_requires=["mycroft-messagebus-client>=0.8.4"],
+    install_requires=get_requirements("requirements.txt"),
     entry_points={
         'console_scripts': [
             'neon_cli_client=neon_cli.__main__:main'
